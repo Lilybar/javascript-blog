375b02f98899e8e2b8fd607a2d0aaa60216f5a45
Lily Bartosik <lidiabartosik7@gmail.com>
2020-07-04 16:13:43 +0200

    add listoftasks

--- js/script.js
+++ js/script.js
@@ -44,10 +44,35 @@
     /* add class 'active' to the correct article */
 
     article.classList.add('active');
+    
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+    }
+
+    generateTitleLinks();
+
   }
-  
+
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+      link.addEventListener('click', titleClickHandler);
   }
