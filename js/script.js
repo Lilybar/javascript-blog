9e68d074c95f17a8d840467477b8800eb5c36920
Lily Bartosik <lidiabartosik7@gmail.com>
2020-07-05 15:14:03 +0200

    create HTML

--- js/script.js
+++ js/script.js
@@ -69,16 +69,18 @@ function generateTitleLinks(){
   /* for each article */
   const articles = document.querySelectorAll(optArticleSelector);
   for(let article of articles){
-  /* get the article id */
-  const articleId = article.getAttribute("id");
-  /* find the title element */
-  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-  console.log(articleTitle);
-  /* get the title from the title element */
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
 
-  /* create HTML of the link */
-
-  /* insert link into titleList */
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /* insert link into titleList */
+    titleList.insertAdjacentHTML = titleList.insertAdjacentHTML + linkHTML;
   }
 }  
 
